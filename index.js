6e87943e03a5be7eb1151e012fd1fad32da33619
Pamela Limoges <pamelalimoges1@gmail.com>
2025-02-04 15:58:14 -0500

    Ajouter routes bases

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-// Creation d'un serveur comme xampp
-const http = require("http");
+// Importations des librairies
+const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
+
+const serveur = express();
 dotenv.config();
 
-const serveur = http.createServer((req, res)=>{
-    if (res.method == "GET" && req.url == "/") {
-        res.end("Accueil");
-    } else {
-        res.end("Patate");
-    }
+// Permettre l'acces au dossier public
+const dossierPublic = path.join(__dirname, "public");
+serveur.use(express.static(dossierPublic));
+
+function authentifier(req, res, next) {
+    console.log("Authentification en cours...");
+    next();
+}
+
+// Routes
+/**
+ * Route servant à recuperer tous les films de la base de données
+ */
+serveur.get("/films", authentifier, (req, res)=> {
+    return res.json({ msg: "ok" });
 });
+
+/**
+ * 
+ */
+serveur.get("/films/:id", (req, res)=> {
+    return res.json({ msg: "ok" });
+});
+
+/**
+ * 
+ */
+serveur.post("/films", (req, res)=> {
+    return res.json({ msg: "ok" });
+});
+
+/**
+ * Route servant à mettre à zero la base de données
+ */
+serveur.post("/films/initialiser", authentifier, (req, res)=> {
+    return res.json({ msg: "Initialisation completer" });
+});
+
+/**
+ * 
+ */
+serveur.put("/films/:id", (req, res)=> {
+    return res.json({ msg: "ok" });
+});
+
+/**
+ * 
+ */
+serveur.delete("/films/:id", (req, res)=> {
+    return res.json({ msg: "ok" });
+});
+
+// Ressources 404
+serveur.use((req, res)=> {
+    res.statusCode = 404;
+    return res.status(404).json({ msg: "Ressource non trouver" });
+});
+
 
 serveur.listen(process.env.PORT, ()=> {
     console.log(`le serveur est demarre sur le port ${process.env.PORT}`);
